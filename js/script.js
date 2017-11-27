5d95bc7df6d0499eb12b2070e296b051a8721a39
LucileV <lverrecchia@gmail.com>
2017-11-27 16:42:25 +0100

    avancement du jour

--- js/script.js
+++ js/script.js
@@ -1,16 +1,2 @@
 
-//alert("Hello! I am an alert box!!");
-
-//menu
-$(document).ready(function(){
-    $(".dropdown").hover(            
-        function() {
-            $('.dropdown-menu', this).stop( true, true ).slideDown("fast");
-            $(this).toggleClass('open');        
-        },
-        function() {
-            $('.dropdown-menu', this).stop( true, true ).slideUp("fast");
-            $(this).toggleClass('open');       
-        }
-    );
-});
+//alert("js connecté");
