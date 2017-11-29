cdc05a1d0ffd92bd6513e2a2a74f7cff5cb2322d
LucileV <lverrecchia@gmail.com>
2017-11-29 16:28:47 +0100

    carousel mais pas trop

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
 //alert("js connecté");
 
-function toTop() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-
-}
+$(document).ready(function() {
+    $('#Carousel').carousel({
+        interval: 5000
+    })
+});
