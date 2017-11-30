1fae8e87a27c75b44e216f7dc6e8b8d125d49a00
LucileV <lverrecchia@gmail.com>
2017-11-30 16:00:38 +0100

    cookies

--- js/script.js
+++ js/script.js
@@ -1,5 +1,27 @@
 
 //alert("js connecté");
+//
+
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#000"
+    },
+    "button": {
+      "background": "#FF8D1B",
+     	  "color" : "white"
+    }
+  },
+  "theme": "classic",
+  "content": {
+    "message": "Ce site utilise des cookies. ",
+    "dismiss": "Je suis d'accord",
+    "link": "Lire plus &gt;",
+    "href": "index.html"
+  }
+})});
+
 
 $(document).ready(function() {
     $('#Carousel').carousel({
