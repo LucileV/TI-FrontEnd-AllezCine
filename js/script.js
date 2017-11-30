3a213563b35f9f59c4225a919188e7e11a942852
LucileV <lverrecchia@gmail.com>
2017-11-30 16:40:44 +0100

    collapse

--- js/script.js
+++ js/script.js
@@ -28,3 +28,13 @@ $(document).ready(function() {
         interval: 5000
     })
 });
+
+
+$(".more").hide();
+jQuery('.button-read-more').click(function () {
+$(this).closest('.less').addClass('active');
+$(this).closest(".less").prev().stop(true).slideDown("1000");
+});
+jQuery('.button-read-less').click(function () {
+$(this).closest('.less').removeClass('active');
+$(this).closest(".less").prev().stop(true).slideUp("1000");
